9f60c6fc2a5488d233b43d59c61ca2e6b860bbac
‘Davide <Simeone.davi@gmail.com>
2018-02-13 08:54:01 +0100

    Initial commit

---
+++ Assets/PlayMusic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMusic : MonoBehaviour {
+
+    void Awake()
+    { //doesn't destroy the gameObject the script is attached to
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+
+
+
+        Invoke("LoadNewScene" ,2f);
+
+	}
+
+    private void LoadNewScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("in m/s")] [SerializeField] float xSpeed = 10f;
+    //[Tooltip("in m")] [SerializeField] float xRange = 7f;
+    [Tooltip("in m/s")] [SerializeField] float ySpeed = 10f;
+    //[Tooltip("in m")] [SerializeField] float yRange = 4f;
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float ClampedX;
+        float ClampedY;
+
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+
+        ClampedX = Mathf.Clamp(rawNewXPos, -6, 7);
+        ClampedY = Mathf.Clamp(rawNewYPos, -3, 4);
+
+
+
+        transform.localPosition = new Vector3(ClampedX, ClampedY, transform.localPosition.z);
+
+       
+
+        print("X  "+ rawNewXPos +" "+ transform.localPosition);
+        print("Y "+ rawNewYPos + " " + transform.localPosition);
+
+	}
+}
